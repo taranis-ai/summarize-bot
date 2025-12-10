eabf11d4e76a0735753ff57ea9a37d96395c91fa
llinauer <l.linauer@gmx.at>
2025-12-10 09:03:39 +0100

    Start sync with bot_template

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
-from summarize_bot import create_app
+from summarize_bot.config import Config
+from taranis_base_bot import create_app
 
-app = create_app()
+app = create_app(Config.PACKAGE_NAME, Config)
