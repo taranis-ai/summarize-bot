0898c035cba2e68ef5b1aea64083ec8b421163fc
llinauer <l.linauer@gmx.at>
2026-03-26 08:01:41 +0100

    Return dict with "summary" key instead of text only

--- summarize_bot/pegasus.py
+++ summarize_bot/pegasus.py
@@ -9,12 +9,12 @@ class Pegasus:
         self.tokenizer = PegasusTokenizer.from_pretrained(self.model_name)
         self.summarizer = PegasusForConditionalGeneration.from_pretrained(self.model_name)
 
-    async def predict(self, text: str) -> str:
+    async def predict(self, text: str) -> dict[str, str]:
         if not text:
             raise ValueError("No text to summarize.")
 
         if len(text) < Config.MAX_LEN:
-            return text
+            return {"summary": text}
 
         inputs = self.tokenizer(text, truncation=True, padding="longest", return_tensors="pt")
         summary_ids = self.summarizer.generate(
@@ -24,4 +24,4 @@ class Pegasus:
             num_beams=Config.NUM_BEAMS,
             early_stopping=True,
         )
-        return self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+        return {"summary": self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)}
